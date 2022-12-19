8a40bb14bc59d40523554e4a3c5aa92b4f5df0ca
Andrey Burtsev <andrey.burtsev@appsforce.io>
2022-12-19 10:50:10 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">test!</div>;
+  return <div className="App">test</div>;
 }
 
 export default App;
