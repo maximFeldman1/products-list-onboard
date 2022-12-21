8a3a6ab4e7ce2c68a1ad1431c47e8f5d44bb59a4
maxim-feldman <maximkaf@gmail.com>
2022-12-21 13:50:01 +0200

    AG

--- src/Pages/Products/index.tsx
+++ src/Pages/Products/index.tsx
@@ -1,12 +1,56 @@
-import React, { useEffect } from "react";
-import { apiUrl } from "../../shared";
 import { ProductService } from "../../shared/services/product-service";
 import { useQuery } from "react-query";
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { render } from "react-dom";
+import { AgGridReact } from "@ag-grid-community/react";
+import "@ag-grid-community/styles/ag-grid.css";
+import "@ag-grid-community/styles/ag-theme-alpine.css";
+import {
+  ColDef,
+  ColGroupDef,
+  GetRowIdFunc,
+  GetRowIdParams,
+  Grid,
+  GridOptions,
+} from "@ag-grid-community/core";
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
 const Products = () => {
   const { data } = useQuery("allProducts", ProductService.getAll);
 
-  return <div>Products</div>;
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [rowData, setRowData] = useState([
+    { id: "c1", make: "Toyota", model: "Celica", price: 35000 },
+    { id: "c2", make: "Ford", model: "Mondeo", price: 32000 },
+    { id: "c8", make: "Porsche", model: "Boxster", price: 72000 },
+    { id: "c4", make: "BMW", model: "M50", price: 60000 },
+    { id: "c14", make: "Aston Martin", model: "DBX", price: 190000 },
+  ]);
+  const [columnDefs, setColumnDefs] = useState<ColDef[]>([
+    { headerName: "Row ID", valueGetter: "node.id" },
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+  ]);
+  const getRowId = useMemo<GetRowIdFunc>(() => {
+    return (params: GetRowIdParams) => params.data.id;
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div style={gridStyle} className="ag-theme-alpine">
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          getRowId={getRowId}
+        ></AgGridReact>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
